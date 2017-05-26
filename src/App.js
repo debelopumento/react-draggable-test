bf9645081145ec21123b41996e237f528d0904ae
debelopumento <imdiye@gmail.com>
2017-05-25 18:09:42 -0700

    added boxed with tracked position

--- src/App.js
+++ src/App.js
@@ -5,16 +5,40 @@ import Draggable from "react-draggable";
 
 class App extends Component {
   state = {
-    activeDrags: 0
+    activeDrags: 0,
+    deltaPosition: {
+      x: 0,
+      y: 0
+    },
+    clientPosition: {
+      x: 0,
+      y: 0
+    }
   };
 
   onStart = () => {
-    console.log(1);
     this.setState({ activeDrags: 1 });
   };
 
   onStop = () => {
     this.setState({ activeDrags: 0 });
+  };
+
+  handleDrag = (event, ui) => {
+    const { x, y } = this.state.deltaPosition;
+    this.setState({
+      deltaPosition: {
+        x: x + ui.deltaX,
+        y: y + ui.deltaY
+      }
+    });
+    console.log(event);
+    this.setState({
+      clientPosition: {
+        x: event.clientX,
+        y: event.clientY
+      }
+    });
   };
 
   render() {
@@ -29,9 +53,36 @@ class App extends Component {
           <h2>React Draggable Test</h2>
         </div>
 
-        <Draggable {...dragHandlers}>
+        <Draggable>
           <div className="box">I can be dragged anywhere</div>
         </Draggable>
+        <Draggable axis="x">
+          <div className="box">I can only be dragged on x axis</div>
+        </Draggable>
+        <Draggable axis="y">
+          <div className="box">I can only be dragged on y axis</div>
+        </Draggable>
+        <Draggable onStart={() => false}>
+          <div className="box">I can't be dragged</div>
+        </Draggable>
+
+        <Draggable onDrag={this.handleDrag}>
+          <div className="box">
+            <div>my deltas:</div>
+            <div>
+              x:
+              {" "}
+              {this.state.deltaPosition.x}
+              , y:
+              {this.state.deltaPosition.y}
+            </div>
+            <div>my position:</div>
+            <div>
+              x: {this.state.clientPosition.x}, y: {this.state.clientPosition.y}
+            </div>
+          </div>
+        </Draggable>
+
       </div>
     );
   }
